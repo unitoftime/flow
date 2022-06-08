22697d020fadcc3c2d07aa65d87fda998958dca1
Jacob Stewart <2606873+unitoftime@users.noreply.github.com>
2022-06-08 10:11:56 -0400

    Added super basic logging

--- log/log.go
+++ log/log.go
@@ -5,6 +5,20 @@ import (
 	"log"
 )
 
+// Ideas:
+// 1. Log everything to a rotating in memory buffer, and dump last n recorded logs on dump
+// 2. Dynamically change log levels at will
+// 3. Compilation flags for release vs debug
+// 4. https://cs.opensource.google/go/go/+/refs/tags/go1.18.2:src/log/log.go;drc=0a1a092c4b56a1d4033372fbd07924dad8cbb50b;l=175
+
+const (
+	LevelTrace = iota
+	LevelInfo
+	LevelWarn
+	LevelError
+	LevelFatal
+)
+
 // From: https://pkg.go.dev/log#pkg-constants
 const (
 	Ldate         = log.Ldate         // the date in the local time zone: 2009/01/23
@@ -16,6 +30,14 @@ const (
 	Lmsgprefix    = log.Lmsgprefix    // move the "prefix" from the beginning of the line to before the message
 	LstdFlags     = Ldate | Ltime     // initial values for the standard logger
 )
+
+// TODO thread safety
+var state struct {
+	level int
+}
+func SetLevel(level int) {
+	state.level = level
+}
 
 func SetFlags(flag int) {
 	log.SetFlags(flag)
@@ -30,18 +52,22 @@ func Println(v ...any) {
 }
 
 func Trace(format string, v ...any) {
+	if state.level > LevelTrace { return }
 	log.Printf("[TRACE] " + format, v...)
 }
 
 func Info(format string, v ...any) {
+	if state.level > LevelInfo { return }
 	log.Printf("[INFO]  " + format, v...)
 }
 
 func Warn(format string, v ...any) {
+	if state.level > LevelWarn { return }
 	log.Printf("[WARN]  " + format, v...)
 }
 
 func Error(format string, v ...any) {
+	if state.level >= LevelError { return }
 	log.Printf("[ERROR] " + format, v...)
 }
 
