0f16e69e884ba06505ec7e6a0a44376c14a7d011
Jacob Stewart <2606873+unitoftime@users.noreply.github.com>
2022-12-06 13:34:08 -0500

    Created a datastrutures package

---
+++ ds/ringbuffer.go
@@ -0,0 +1,26 @@
+package ds
+
+type RingBuffer[T any] struct {
+	idx int
+	buffer []T
+}
+func NewRingBuffer[T any](length int) *RingBuffer[T] {
+	return &RingBuffer[T]{
+		idx: 0,
+		buffer: make([]T, length),
+	}
+}
+
+func (b *RingBuffer[T]) Add(t T) {
+	b.buffer[b.idx] = t
+	b.idx = (b.idx + 1) % len(b.buffer)
+}
+
+// TODO - Maybe convert this to an iterator
+func (b *RingBuffer[T]) Buffer() []T {
+	ret := make([]T, len(b.buffer))
+	firstSliceLen := len(b.buffer) - b.idx
+	copy(ret[:firstSliceLen], b.buffer[b.idx:len(b.buffer)])
+	copy(ret[firstSliceLen:], b.buffer[0:b.idx])
+	return ret
+}
