efdbbaad7363f57a5ab3e47adb9e5ac56114def2
Jacob Stewart <2606873+unitoftime@users.noreply.github.com>
2023-06-29 12:41:56 -0400

    Added velocity functions

--- phy2/physics.go
+++ phy2/physics.go
@@ -6,6 +6,7 @@ import (
 
 //cod:struct
 type Pos Vec2
+
 func (v Pos) Add(v2 Pos) Pos {
 	return Pos(Vec2(v).Add(Vec2(v2)))
 }
@@ -40,6 +41,38 @@ func (v Pos) Angle() float64 {
 
 //cod:struct
 type Vel Vec2
+
+func (v Vel) Add(v2 Vel) Vel {
+	return Vel(Vec2(v).Add(Vec2(v2)))
+}
+
+func (v Vel) Sub(v2 Vel) Vel {
+	return Vel(Vec2(v).Sub(Vec2(v2)))
+}
+
+func (v Vel) Norm() Vel {
+	return Vel(Vec2(v).Norm())
+}
+
+func (v Vel) Dot(u Vel) float64 {
+	return Vec2(v).Dot(Vec2(u))
+}
+
+func (v Vel) Len() float64 {
+	return Vec2(v).Len()
+}
+
+func (v Vel) Scaled(s float64) Vel {
+	return Vel(Vec2(v).Scaled(s))
+}
+
+func (v Vel) Rotated(radians float64) Vel {
+	return Vel(Vec2(v).Rotated(radians))
+}
+
+func (v Vel) Angle() float64 {
+	return math.Atan2(v.Y, v.X)
+}
 
 //cod:struct
 type Scale Vec2
