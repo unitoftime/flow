e1e86ad9ddf0e38bdac11dc520ed6ae6fcd0923d
Jacob <2606873+unitoftime@users.noreply.github.com>
2024-03-21 10:38:20 +0000

    Add serve tool

---
+++ tools/serve/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"flag"
+	"net/http"
+)
+
+func main() {
+	port := flag.String("p", "8081", "port to serve on")
+	directory := flag.String("d", ".", "the directory of static file to host")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
+
+	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
