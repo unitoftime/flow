540b763d83ee04d1ae5667c989ea71191e737a07
Jacob <2606873+unitoftime@users.noreply.github.com>
2024-04-07 09:33:26 -0400

    Fix newTab behavior

--- browser/open_wasm.go
+++ browser/open_wasm.go
@@ -1,4 +1,5 @@
 //go:build js || wasm
+
 package browser
 
 import (
@@ -11,9 +12,10 @@ var location = window.Get("location")
 func Open(url string, newTab bool) error {
 	if newTab {
 		// "location=yes,height=570,width=520,scrollbars=yes,status=yes")
-		window.Call("open", url, "_blank", "location=yes,scrollbars=yes,status=yes")
+		// window.Call("open", url, "_blank", "location=yes,scrollbars=yes,status=yes")
+		window.Call("open", url, "_blank") // Open as new tab
 	} else {
-		window.Call("open", url, "_self")
+		window.Call("open", url, "_blank", "location=yes,scrollbars=yes,status=yes") // Open as window
 		// try to use just window.location?
 		// location.Set("href", url)
 	}
